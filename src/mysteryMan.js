c92d837280b4afdfd69297b3b7819a26e8eb1d2c
Anar <anarram28@gmail.com>
2026-06-23 00:01:14 +0300

    Deploy: src/mysteryMan.js

--- src/mysteryMan.js
+++ src/mysteryMan.js
@@ -1,6 +1,6 @@
 // Mystery Man interaction system for Void Wanderer
 
-import { spawnSmoke, spawnSparkles, spawnFloatingText } from './particles.js?v=21';
+import { spawnSmoke, spawnSparkles, spawnFloatingText } from './particles.js?v=22';
 
 export class MysteryManNPC {
     constructor(x, y) {
