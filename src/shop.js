62bf72851510a92254aa4f5527c646f5c4839fc8
Anar <anarram28@gmail.com>
2026-06-23 01:31:58 +0300

    Deploy: src/shop.js

--- src/shop.js
+++ src/shop.js
@@ -1,6 +1,6 @@
 // Shopkeeper Merchant NPC system for Void Wanderer
 
-import { spawnSmoke, spawnSparkles, spawnFloatingText } from './particles.js?v=22';
+import { spawnSmoke, spawnSparkles, spawnFloatingText } from './particles.js?v=23';
 
 export class ShopkeeperNPC {
     constructor(x, y) {
